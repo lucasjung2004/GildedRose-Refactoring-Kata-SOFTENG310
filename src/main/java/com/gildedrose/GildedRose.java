c85b82c56228cc9109844238a449aad8dc2f0d93
lucasjung2004 <lucas5822@gmail.com>
2025-08-14 14:50:37 +1200

    Refactored code to have less nesting and improved readability

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,14 +9,9 @@ class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if (!items[i].name.equals("Aged Brie")
-          && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items[i].quality > 0) {
-          if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            items[i].quality = items[i].quality - 1;
-          }
-        }
-      } else {
+        if (isNormalItem(items[i]) && items[i].quality > 0) {
+            items[i].quality--;
+          } else {
         increaseQualityIfPossible(items[i]);
         if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
           if (items[i].sellIn < 11) {
@@ -28,6 +23,12 @@ class GildedRose {
         }
       }
     }
+
+    private boolean isNormalItem(Item item) {
+        return !item.name.equals("Aged Brie")
+            && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+            && !item.name.equals("Sulfuras, Hand of Ragnaros");
+      }
 
     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
       items[i].sellIn = items[i].sellIn - 1;
